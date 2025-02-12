a0cda4efd83c1c2ced3fa4a39f917a1246525d19
thauveel <thauvyl@gmail.com>
2025-02-12 20:09:05 +0500

    feat:redirect by user type

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,11 +29,20 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+        
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        
+
+        if (Auth::user()->is_admin) {
+            return redirect()->intended(route('dashboard', absolute: false));
+        } else {
+            return redirect()->intended(route('user.dashboard', absolute: false));
+        }
+
+        
     }
 
     /**
