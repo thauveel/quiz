e0c95bf0e938831c4bedef6898b5bd6d434363fc
thauveel <thauvyl@gmail.com>
2025-02-12 19:29:22 +0500

    add:middleware is_admin

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class isAdmin
@@ -15,10 +16,11 @@ class isAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!auth()->user()->is_admin) {
-            abort(403);
-        }
         
-        return $next($request);
+        if (Auth::check() && Auth::user()->is_admin) {
+            return $next($request);
+        }
+
+        return redirect('/')->with('error', 'Access denied.');
     }
 }
